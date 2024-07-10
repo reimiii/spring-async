3b68f5ce749dc47485f6e2f2b952233c092f7351
Hilmi AM <imiia75775@proton.me>
2024-07-10 22:06:38 +0700

    Async

---
+++ src/main/java/franxx/code/async/HelloAsync.java
@@ -0,0 +1,20 @@
+package franxx.code.async;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+
+@Slf4j
+@Component
+public class HelloAsync {
+
+  @Async
+  @SneakyThrows
+  public void hello() {
+    Thread.sleep(Duration.ofSeconds(2));
+    log.info("hello after 2sec - {}", Thread.currentThread());
+  }
+}
---
+++ src/test/java/franxx/code/async/HelloAsyncTest.java
@@ -0,0 +1,30 @@
+package franxx.code.async;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+@SpringBootTest
+class HelloAsyncTest {
+
+  @Autowired
+  private HelloAsync helloAsync;
+
+  @Test
+  @SneakyThrows
+  void helloAsync() {
+    for (int i = 0; i < 10; i++) {
+      helloAsync.hello();
+    }
+
+    log.info("after call helloAsync()");
+    Thread.sleep(Duration.ofSeconds(5));
+  }
+}
